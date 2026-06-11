1ccafda6dc92337a9ab32aeae3e2a263e0814178
Mathilde Lykke <mathilly@stud.ntnu.no>
2026-06-11 15:28:04 +0200

    Login now moves to homepage

--- application_erasmhealth/lib/main.dart
+++ application_erasmhealth/lib/main.dart
@@ -1,20 +1,16 @@
-import 'package:application_erasmhealth/screens/Cocktail_Animation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'package:application_erasmhealth/screens/homePage.dart';
 import 'package:application_erasmhealth/providers/app_state.dart';
 import 'package:application_erasmhealth/utils/impact.dart';
 import 'package:application_erasmhealth/screens/Cocktail_Animation.dart';
+import 'package:application_erasmhealth/screens/homePage.dart';
+import 'package:application_erasmhealth/screens/LoginPage.dart';
 
-
-/// Entry point of the app
-/// // Initializes the Provider for state management and runs the app
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   final appState = AppState(Impact());
-
   await appState.restoreLoginState();
 
   runApp(
@@ -25,8 +21,15 @@ void main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _splashDone = false;
 
   @override
   Widget build(BuildContext context) {
@@ -34,14 +37,15 @@ class MyApp extends StatelessWidget {
       title: 'Erasmhealth App',
       debugShowCheckedModeBanner: false,
       home: Consumer<AppState>(
-          builder: (context, appState, _) {
-
-          if (appState.isLoggedIn) {
-              return const HomePage();
+        builder: (context, appState, _) {
+          if (appState.isLoggedIn) return const HomePage();
+          if (!_splashDone) {
+            return CocktailAnimation(
+              onDone: () => setState(() => _splashDone = true),
+            );
           }
-
-          return const CocktailAnimation();
-          },
+          return const LoginPage();
+        },
       ),
     );
   }
